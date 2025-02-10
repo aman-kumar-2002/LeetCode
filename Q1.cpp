32400bb0bf8b9d03ebaf263799c04e1e43ad3d41
Aman Kumar <restricted.aman@gmail.com>
2025-02-10 22:53:01 +0530

    k

--- Q1.cpp
+++ Q1.cpp
@@ -4,7 +4,7 @@ return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
 
-You can return the answer in any order.*/
+You can return the answer in any order. */
 
 #include <vector>
 #include <unordered_map>
