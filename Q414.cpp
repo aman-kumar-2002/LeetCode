b125ad9c7725e89ac69d599c7ee76b7c7ebfafb4
Aman Kumar <restricted.aman@gmail.com>
2025-03-25 02:24:23 +0530

    ll

---
+++ Q414.cpp
@@ -0,0 +1,22 @@
+/*Given an integer array nums, return the third distinct maximum number in this array.
+If the third maximum does not exist, return the maximum number.*/
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int thirdMax(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        if (nums.size() >= 3)
+        {
+            return nums[nums.size() - 3];
+        }
+
+        return nums[nums.size() - 1];
+    }
+};
